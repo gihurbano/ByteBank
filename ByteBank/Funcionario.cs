81ca9dac6d11d58dd37421ab72d1228b4ff96f8e
Gisele Urbano <gisele_urbano@hotmail.com>
2021-09-25 14:40:05 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Funcionario
+    {
+
+        public String Nome { get; set; }
+        public String CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double ObterBonificacao() {
+            
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario gisele = new Funcionario();
+
+            gisele.Nome = "Gisele";
+            gisele.CPF = "045.224.429.39";
+            gisele.Salario = 2000;
+
+            Console.WriteLine(gisele.Nome);
+            Console.WriteLine(gisele.ObterBonificacao());
+            Console.ReadLine();
+
+        }
+    }
+}
